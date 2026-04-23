65f07a96978478422ad0e7d4ec869b5cc164d426
bhavy0024 <bhavyahirpara8@gmail.com>
2026-04-23 16:30:55 +0530

    Integrated input-based ledger with versioning and fraud detection

--- main.cpp
+++ main.cpp
@@ -17,6 +17,31 @@ Node* transaction(Node* prev, int n, int from, int to, int amount)
 }
 int32_t main()
 {
-    cout << "Ledger System Started\n";
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    vector<Node*> versions;
+    Node* root0 = build(arr, 0, n - 1);
+    versions.push_back(root0);
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int from, to, amount;
+        cin >> from >> to >> amount;
+        Node* new_root = transaction(versions.back(), n, from, to, amount);
+        detect_fraud(versions.back(), new_root, n);
+        versions.push_back(new_root);
+    }
+    // queries after all transactions
+    int k;
+    cin >> k; // number of queries
+     while (k--)
+    {
+        int version, l, r;
+        cin >> version >> l >> r;
+        cout << query(versions[version], 0, n - 1, l, r) << "\n";
+    }
     return 0;
 }
