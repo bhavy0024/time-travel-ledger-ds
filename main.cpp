f4d2d85db650844b3e5ac3c351fb3f8c0669092a
bhavy0024 <bhavyahirpara8@gmail.com>
2026-04-23 16:06:06 +0530

    Added transaction system for fund transfer
    Add transaction function to handle fund transfers between accounts.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 #define int long long
 #include "persistenttree.h"
+Node* transaction(Node* prev, int n, int from, int to, int amount)
+{
+    int balfrom = query(prev, 0, n - 1, from, from);
+    int balto = query(prev, 0, n - 1, to, to);
+    if (balfrom < amount)
+    {
+        cout << "Transaction failed (insufficient funds)\n";
+        return prev;
+    }
+    Node* temp = update(prev, 0, n - 1, from, balfrom - amount);
+    Node* newroot = update(temp, 0, n - 1, to, balto + amount);
+    return newroot;
+}
 int32_t main()
 {
     cout << "Ledger System Started\n";
