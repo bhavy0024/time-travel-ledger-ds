3f3c75fff50a02b4d553edc56d329653f6423da5
bhavy0024 <bhavyahirpara8@gmail.com>
2026-04-23 15:57:46 +0530

    Implemented segment tree build function

--- persistenttree.h
+++ persistenttree.h
@@ -4,3 +4,14 @@ struct Node {
     Node* right;
     Node(int v) : val(v), left(NULL), right(NULL) {}
 };
+Node* build(vector<int> &arr, int l, int r)
+{
+    if (l == r) return new Node(arr[l]);
+    int mid = (l + r) / 2;
+    Node* left = build(arr, l, mid);
+    Node* right = build(arr, mid + 1, r);
+    Node* root = new Node(left->val + right->val);
+    root->left = left;
+    root->right = right;
+    return root;
+}
