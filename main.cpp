6703b20208c7386d15257a7558a8f163512b628e
Bhavy Hirapara <bhavyahirpara8@gmail.com>
2026-06-26 18:06:23 +0530

    Refactor transaction and query handling in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,41 +2,48 @@
 using namespace std;
 #define int long long
 #include "persistenttree.h"
+vector<Node*> versions;
 int32_t main()
 {
     int n;
     cin >> n;
-    vector<int> arr(n);
+    vector<long long> arr(n);
     for (int i = 0; i < n; i++) cin >> arr[i];
-    vector<Node*> versions;
     Node* root0 = build(arr, 0, n - 1);
-    versions.push_back(root0);
+    versions.push_back(root0);  
     int q;
     cin >> q;
     while (q--)
     {
-        int from, to, amount;
-        cin >> from >> to >> amount;
-        Node* new_root = transaction(versions.back(), n, from, to, amount);
-        if (new_root != versions.back())
+        string type;
+        cin >> type;
+        if (type == "txn")
         {
-            detectfraud(versions.back(), new_root, n);
-            versions.push_back(new_root);
+            int from, to;
+            long long amount;
+            cin >> from >> to >> amount;
+            Node* new_root = transaction(versions.back(), n, from, to, amount);
+            if (new_root != versions.back())
+            {
+                detectfraud(versions.back(), new_root, n);
+                versions.push_back(new_root);
+            }
         }
-    }
-    // queries after all transactions
-    int k;
-    cin >> k; // number of queries
-     while (k--)
-    {
-        int version, l, r;
-        cin >> version >> l >> r;
-        if (version < 0 || version >= versions.size())
+        else if (type == "query")
         {
-            cout << "Invalid version\n";
-            continue;
+            int version, l, r;
+            cin >> version >> l >> r;
+            if (version < 0 || version >= (int)versions.size())
+            {
+                cout << "Invalid version\n";
+                continue;
+            }
+            cout << query(versions[version], 0, n - 1, l, r) << "\n";
         }
-        cout << query(versions[version], 0, n - 1, l, r) << "\n";
+        else if (type == "status")
+        {
+            cout << "Total versions: " << versions.size() << "\n";
+        }
     }
     return 0;
 }
